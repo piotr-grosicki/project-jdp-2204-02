1b1c0ba2ffedf1111807de0184951a4d72005097
Aleksandra <ola.odzierzynska@gmail.com>
2022-04-03 20:44:20 +0200

    Dodanie endpointów w UserController

--- src/main/java/com/kodilla/ecommercee/controller/UserController.java
+++ src/main/java/com/kodilla/ecommercee/controller/UserController.java
@@ -1,4 +1,28 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/v1/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public UserDto addUser(@RequestBody UserDto userDto) {
+        return new UserDto();
+    }
+
+    @PutMapping(value = "block_user/{userId}")
+    public void blockUser(@PathVariable int userId) {
+    }
+
+    @PutMapping(value="createKey/{userId}")
+    public void createKey(@PathVariable int userId){
+
+    }
+
 }
