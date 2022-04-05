66780404fcb419b9d1c2376737e14d2e28610779
Szymon Wizdorf <szymekwizdorf@gmail.com>
2022-04-05 20:32:17 +0200

    dodano GroupRepository

---
+++ src/main/java/com/kodilla/ecommercee/repository/GroupRepository.java
@@ -0,0 +1,22 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.Group;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+@Repository
+public interface GroupRepository extends JpaRepository<Group, Long> {
+
+    List<Group> findAll();
+    Optional<Group> findByGroupId(Long id);
+    Group save(Group group);
+    void deleteByGroupId(Long id);
+
+
+
+}
