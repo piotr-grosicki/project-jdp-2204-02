43caf4cddd37047f36ae1a38c5ecd1c1ef9b466a
Damian Bernat <damianbernat@wp.pl>
2022-04-07 21:39:36 +0200

    Przenioslem CartController do wlasciwego folderu

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,38 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.dto.CartDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/carts")
+@RequiredArgsConstructor
 public class CartController {
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public void createCart(@RequestBody CartDto cartDto) {
+    }
+
+    @GetMapping(value = "{cartId}")
+    public CartDto getCart(@PathVariable Long cartId) {
+        return new CartDto();
+    }
+
+    @GetMapping
+    public List<CartDto> getAllCarts() {
+        return new ArrayList<>();
+    }
+
+    @PutMapping
+    public CartDto editCart(@RequestBody CartDto cartDto) {
+        return new CartDto();
+    }
+
+    @DeleteMapping(value = "{cartId}")
+    public void deleteCart(@PathVariable Long cartId) {
+    }
 }
