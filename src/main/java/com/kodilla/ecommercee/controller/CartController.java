e339e92b412fda6804a40acd5f9693afae0b0414
Damian Bernat <damianbernat@wp.pl>
2022-04-15 14:10:59 +0200

    poprawiony endpoint delete, dodane endpointy do dodawania i usuwania produktow

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -2,8 +2,11 @@ package com.kodilla.ecommercee.controller;
 
 import com.kodilla.ecommercee.Exceptions.CartNotFoundException;
 import com.kodilla.ecommercee.domain.Cart;
+import com.kodilla.ecommercee.domain.Product;
 import com.kodilla.ecommercee.dto.CartDto;
+import com.kodilla.ecommercee.dto.ProductDto;
 import com.kodilla.ecommercee.mapper.CartMapper;
+import com.kodilla.ecommercee.mapper.ProductMapper;
 import com.kodilla.ecommercee.service.DbCartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
@@ -19,6 +22,7 @@ public class CartController {
 
     private final DbCartService service;
     private final CartMapper cartMapper;
+    private final ProductMapper productMapper;
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> createCart(@RequestBody CartDto cartDto) {
@@ -43,6 +47,23 @@ public class CartController {
         Cart cart = cartMapper.mapToCart(cartDto);
         Cart editedCart = service.saveCart(cart);
         return ResponseEntity.ok(cartMapper.mapToCartDto(editedCart));
+    }
+
+    @PutMapping(value = "/add/{cartId}")
+    public ResponseEntity<CartDto> addProductToCart(@PathVariable Long cartId, @RequestBody ProductDto productDto) throws CartNotFoundException {
+        Cart cart = service.getCartById(cartId);
+        Product product = productMapper.mapToProduct(productDto);
+        cart.getProducts().add(product);
+        Cart updatedCart = service.saveCart(cart);
+        return ResponseEntity.ok(cartMapper.mapToCartDto(updatedCart));
+    }
+
+    @DeleteMapping(value = "/delete/{cartId}")
+    public ResponseEntity<Void> deleteProductFromCart(@PathVariable Long cartId, @RequestParam("item") int item) throws CartNotFoundException {
+        Cart cart = service.getCartById(cartId);
+        cart.getProducts().remove(item);
+        service.saveCart(cart);
+        return ResponseEntity.ok().build();
     }
 
     @DeleteMapping(value = "{cartId}")
